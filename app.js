f29d16656b2becca624a70ec25bedbb4ead13fc0
raohxxx <91131926+raohxxx@users.noreply.github.com>
2025-01-22 15:22:39 -0300

    Add files via upload
    agregando archivos numero secreto

--- app.js
+++ app.js
@@ -1,1 +1,5 @@
-console.log(" probando el app")
+let titulo=document.querySelector('h1');
+titulo.innerHTML='Juego del numero secreto';
+let parrafo=document.querySelector('p');
+parrafo.innerHTML= 'Escoje un numero del 1 al 10';
+function intentoDeUsuario() {alert('click desde el boton');}
