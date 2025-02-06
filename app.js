c39a599b67002401f59091e0440fc2b98521bb18
Rafael <rafael.osunahernandez@gmail.com>
2025-02-06 18:48:42 -0300

    se modifica la funcion intento usuario

--- app.js
+++ app.js
@@ -2,4 +2,5 @@ let titulo=document.querySelector('h1');
 titulo.innerHTML='Juego del numero secreto';
 let parrafo=document.querySelector('p');
 parrafo.innerHTML= 'Escoje un numero del 1 al 10';
-function intentoDeUsuario() {alert('click desde el boton');}
+let entrada=document.querySelector('input')
+function intentoDeUsuario() {alert(`click desde el boton con el numero ${entrada.value}`);}
