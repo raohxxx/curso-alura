2a83213082c46594961f2bce16fca1d0f55f1eda
Rafael Osuna <91131926+raohxxx@users.noreply.github.com>
2025-02-06 18:51:33 -0300

    se agrega dos Puntos

--- app.js
+++ app.js
@@ -3,4 +3,4 @@ titulo.innerHTML='Juego del numero secreto';
 let parrafo=document.querySelector('p');
 parrafo.innerHTML= 'Escoje un numero del 1 al 10';
 let entrada=document.querySelector('input')
-function intentoDeUsuario() {alert(`click desde el boton con el numero ${entrada.value}`);}
+function intentoDeUsuario() {alert(`click desde el boton con el numero : ${entrada.value}`);}
